fa5ff7c2ecba29689c170f6db475a9a7c81e7c6e
mrsekut <k.cloudspider@gmail.com>
2026-02-04 23:40:47 +0900

    fix: プロジェクト名をlast_import.txtに含める

--- src/lastImportTime.ts
+++ src/lastImportTime.ts
@@ -1,21 +1,27 @@
 import { FileSystem } from '@effect/platform';
-import { Effect, Option } from 'effect';
+import { Config, Effect, Option } from 'effect';
 
-const LAST_IMPORT_FILE = 'last_import.txt';
+const lastImportFile = Effect.gen(function* () {
+  const source = yield* Config.string('SOURCE_PROJECT_NAME');
+  const dest = yield* Config.string('DESTINATION_PROJECT_NAME');
+  return `last_import_${source}_${dest}.txt`;
+});
 
 export const getLastImportTime = Effect.gen(function* () {
   const fs = yield* FileSystem.FileSystem;
+  const file = yield* lastImportFile;
 
-  const exists = yield* fs.exists(LAST_IMPORT_FILE);
+  const exists = yield* fs.exists(file);
   if (!exists) {
     return Option.none<number>();
   }
-  const content = yield* fs.readFileString(LAST_IMPORT_FILE);
+  const content = yield* fs.readFileString(file);
   return Option.some(parseInt(content, 10));
 });
 
 export const saveLastImportTime = (time: number) =>
   Effect.gen(function* () {
     const fs = yield* FileSystem.FileSystem;
-    yield* fs.writeFileString(LAST_IMPORT_FILE, time.toString());
+    const file = yield* lastImportFile;
+    yield* fs.writeFileString(file, time.toString());
   });
